c32d8d4f8c395c3c95896006eb6b32a7891cb538
Nicholas Watts <thekilowat@gmail.com>
2024-08-23 03:04:55 -0500

    Initial Commit

---
+++ LokmaLives/LokmaLivesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LokmaLivesApp.swift
+//  LokmaLives
+//
+//  Created by Nick Watts on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LokmaLivesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
